45928587fd4df0258fc0f82519ca8d2b1fef53a2
DevEnock <enockomondi305@gmail.com>
2022-01-12 16:14:04 +0300

    query params

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +7,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'routing-demo';
   currentRoute!: string;
+  title!: string;
+  name!: string;
 
-  constructor(private router: Router){}
+  constructor(private router: Router, private route: ActivatedRoute){}
+
+  ngOnInit(){
+    this.route.queryParams
+    .subscribe(params =>{
+      console.log(params);
+    })
+  }
 
   displaySecondComponent(){
-    this.router.navigate(['/second-component'], {queryParams: {title: 'secondcomponent'}})
+    this.router.navigate(['/second-component'], {queryParams: {title: 'secondcomponent', name: 'secondpage'}})
   }
 
   logQueryParams(){}
